6feaba76ec61036f7d3ec6adffbe1f4052e93ee6
stephenhadi <stephenhadi123@gmail.com>
2019-10-23 00:15:00 +0700

    player function

--- app/src/main/java/com/example/tubes2/ThreadHandler.java
+++ app/src/main/java/com/example/tubes2/ThreadHandler.java
@@ -11,7 +11,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 /*
-
  */
 public class ThreadHandler extends Handler {
     private IMainActivity iMainActivity;
@@ -22,33 +21,25 @@ public class ThreadHandler extends Handler {
     @Override
     public void handleMessage(Message msg){
         if(msg.what == 0){
-            ObjHolder objHolder = (ObjHolder) msg.obj;
-            iMainActivity.drawPlayer(objHolder.getN(),objHolder.getN2());
+            int[] n = (int[])msg.obj;
+            Log.d("handle message",n[0]+" "+n[1]);
+            iMainActivity.drawPlayer(n[0],n[1]);
         }
-
+        else if(msg.what == 1){
+            int[] n =(int[])msg.obj;
+            this.iMainActivity.drawBullet(n[0],n[1]);
+        }
     }
-    public void draw(int[] n,int[][] n2){
+    public void draw(int[] n){
         Message msg = new Message();
         msg.what = 0; // value for player
-        msg.obj = new ObjHolder(n,n2);
+        msg.obj = n;
         this.sendMessage(msg);
     }
-
-    /*
-     * obj holder menyimpan file x dan y untuk player,enemy,bullet
-     */
-    private class ObjHolder{
-        private int[] n;
-        private int[][] n2;
-        public ObjHolder(int[] n,int[][] n2){
-            this.n = n;
-            this.n2 = n2;
-        }
-        public int[] getN(){
-            return this.n;
-        }
-        public int[][] getN2(){
-            return this.n2;
-        }
+    public void drawBullet(int[] n ){
+        Message msg = new Message();
+        msg.what = 1; // value for bullet
+        msg.obj = n;
+        this.sendMessage(msg);
     }
 }
